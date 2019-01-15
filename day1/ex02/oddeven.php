46af3e756b0e5eaaeccb7181c54d00006eddc271
ishtvan-lucor <ishtvan.koloshynsky@gmail.com>
2019-01-15 19:32:45 +0200

    Check add or even number

---
+++ day1/ex02/oddeven.php
@@ -0,0 +1,16 @@
+<?php
+    while (TRUE) {
+		print("Enter a number: ");
+		$str = fgets(STDIN);
+		if (!$str) {
+			print("\n");
+			break;
+		}
+		$str = trim($str);
+		if (!is_numeric($str))
+			print("'$str' is not a number\n");
+		else if ($str % 2 == 0)
+			print("The number $str is even\n");
+		else
+			print("The number $str is odd\n");
+	}
